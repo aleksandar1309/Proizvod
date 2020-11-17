a25fbf65030a465bb459e4e21fac260c3355b67b
aleksandar1309 <48052073+aleksandar1309@users.noreply.github.com>
2020-11-17 14:20:20 +0100

    Add files via upload

---
+++ Proizvod.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZadatakProizvod.Models
+{
+    [Table("Proizvod")]
+    public class Proizvod
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Unesite naziv")]
+        [StringLength(30,ErrorMessage ="Može biti uneto najviše 30 karaktera")]
+        public string Naziv { get; set; }
+
+        [Required(ErrorMessage = "Unesite opis proizvoda")]
+        [StringLength(30, ErrorMessage = "Može biti uneto najviše 30 karaktera")]
+        public string Opis { get; set; }
+
+        [Required(ErrorMessage = "Unesite Kategoriju proizvoda")]
+        [StringLength(30, ErrorMessage = "Može biti uneto najviše 30 karaktera")]
+        public string Kategorija { get; set; }
+
+        [Required(ErrorMessage = "Unesite proizvodjača")]
+        [StringLength(30, ErrorMessage = "Može biti uneto najviše 30 karaktera")]
+        public string Proizvodjac { get; set; }
+
+        [Required(ErrorMessage = "Unesite dobavljača")]
+        [StringLength(30, ErrorMessage = "Može biti uneto najviše 30 karaktera")]
+        public string Dobavljac { get; set; }
+
+        [Required(ErrorMessage = "Unesite cenu")]
+        public int Cena { get; set; }
+
+
+
+     
+
+    }
+
+
+}
